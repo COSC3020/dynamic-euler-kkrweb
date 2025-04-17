78fd9ddbdcc93c2bd22ae2e405a65c18bd04afbb
Kane Kriz <kanemkriz@gmail.com>
2025-04-17 15:22:32 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,36 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+// Kane Kriz
+// UWYO COSC 3020 Algorithms
+// Dynamic Euler - primary js file
+// 17 April 2025
+//
+//
+
+
+//
+
+
+function factorial(n) 
+{
+    if(n === 0) 
+    {
+        return 1;
+    }
+        
+    else()
+    {
+        return n * factorial(n - 1);
+    }
 }
 
-function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+function e(n)
+{
+    if(n === 0)
+    {
+        return 1;
+    }
+        
+    else()
+    {
+        return 1.0 / factorial(n) + e(n - 1);
+    }
 }
