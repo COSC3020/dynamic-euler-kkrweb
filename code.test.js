f90a11fd51ef8585ef69fb075c2ae800136d1916
Kane Kriz <kanemkriz@gmail.com>
2025-04-21 14:47:10 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,6 +1,16 @@
+// Kane Kriz
+// UWYO COSC 3020 Algorithms
+// Dynamic Euler Exercise - test js code
+// 21 April 2025
+//
+//
+
+//original provided code
 const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+//added code
